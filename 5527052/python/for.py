faef3af1d34565002b2e90353258e0e69b59bb0b
Sijia6 <156685319+Sijia6@users.noreply.github.com>
2024-01-25 00:25:14 +0000

    Add files via upload

---
+++ 5527052/python/for.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print("Hello, World!")
+
+for i in range(5):
+    print("This is inside the loop.")
+    print("This will also repeat in the loop.")
+
+counter = 0
+while counter < 5:
+    print("counter is",counter)
+    counter += 1 
---
+++ 5527052/python/roll_dice.py
@@ -0,0 +1,10 @@
+import random
+
+def roll_dice():
+    return random.randint(1, 7)
+
+print(roll_dice())
+print(roll_dice())
+print(roll_dice())
+print(roll_dice())
+print(roll_dice())
