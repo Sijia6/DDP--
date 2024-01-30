87411bcb5fc4a567786d7fff3bf6ddfd87ed6db0
Sijia6 <156685319+Sijia6@users.noreply.github.com>
2024-01-30 23:40:35 +0000

    Add files via upload

---
+++ 5527052/python/prime_factors.py
@@ -0,0 +1,18 @@
+def prime_factors(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+        
+    for i in range(3, int(n**0.5)+1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    
+    if n > 2:
+        factors.append(n)
+        
+    return factors
+number = 100
+factors = prime_factors(number)
+print(factors)  
