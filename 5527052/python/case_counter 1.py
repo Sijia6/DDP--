d02c881e203c310c0a2c813d9aada92e7f7eea20
Sijia6 <156685319+Sijia6@users.noreply.github.com>
2024-02-04 20:35:40 +0000

    Delete 5527052/python/case_counter 1.py

--- 5527052/python/case_counter 1.py
+++
@@ -1,7 +0,0 @@
-def case_counter(text):
-    uppercase_count = sum(1 for char in text if char.isupper())
-    lowercase_count = sum(1 for char in text if char.islower())
-    return uppercase_count, lowercase_count
-print(case_counter("PYTHON"))
-print(case_counter("Hello,world"))
-print(case_counter("Python"))
